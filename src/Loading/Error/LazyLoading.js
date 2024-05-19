91d81a4e2ca8605f8a55e35251d131b9dabae043
Davidayo16 <idowuodimayo@gmail.com>
2024-05-19 21:16:51 +0100

    commit

---
+++ src/Loading/Error/LazyLoading.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LazyLoading = () => {
+  return (
+
+      <div class="center">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
+   
+  );
+};
+
+export default LazyLoading;
